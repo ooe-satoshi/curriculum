f5bbf9e94d08df2c1b82c9823acc57715f14baf0
ooe-satoshi <bclarence747@gmail.com>
2020-07-08 18:54:07 +0900

    課題テスト
    2章チェックテスト

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,29 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+    private final String lastName;
+    private final String firstName;
+
+    public Check() {
+        this.lastName = "大恵";
+        this.firstName = "悟志";
+    }
+
+    private void printName(String lastName, String firstName) {
+        System.out.println(lastName + " " + firstName);
+    }
+
+    public static void main(String[] args) {
+        Check check = new Check();
+        Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+        Pet rob = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+
+        System.out.print("printNameメソッド → ");
+        check.printName(check.lastName, check.firstName);
+        pet.introduce();
+        rob.introduce();
+    }
+
+}
